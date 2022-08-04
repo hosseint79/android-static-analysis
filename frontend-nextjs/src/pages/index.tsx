a0a2787d809afcf9cb0d79c24ccc696716e971f1
Hossein <hossein.w7979@gmail.com>
2022-08-04 16:58:23 +0430

    feat: add sppiner for loading state

--- frontend-nextjs/src/pages/index.tsx
+++ frontend-nextjs/src/pages/index.tsx
@@ -1,24 +1,37 @@
 import type { NextPage } from 'next'
 import React, { useState } from 'react'
 import axios from "axios"
+import {useDropzone} from 'react-dropzone';
+import { useRouter } from 'next/router';
 
 const Home: NextPage = () => {
-  const [file,setFile] = useState<any>()
   const [loading,setLoading] = useState(false)
+  const router = useRouter()
 
-  const onchangeHandle = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setFile(e.target.files)
-  }
+  const {
+    acceptedFiles,
+    getRootProps,
+    getInputProps
+  } = useDropzone();
+
+  const acceptedFileItems = acceptedFiles.map((file:any) => (
+    <li key={file.path} className="py-3 px-5 rounded-md bg-slate-300">
+      {file.path} - <span className='text-orange-500'>
+       ( {file.size} bytes )
+      </span>
+    </li>
+  ));
+
 
   async function handleClick(){
     setLoading(true)
     const data = new FormData()
-    data.append("file",file[0])
+    data.append("file",acceptedFiles[0])
     
     try {
       const result = await axios.post("http://127.0.0.1:8000/upload/",data)  
       const scanResult = await axios.get("http://127.0.0.1:8000/scan?fileName=" + result.data.fileName)
-      console.log(scanResult)
+      router.push("/panel/project/")
     } catch (error) {
       
     }
@@ -27,14 +40,45 @@ const Home: NextPage = () => {
   }
 
   return (
-   <div className='bg-red-400 w-full h-60'>
-      <input type="file" onChange={onchangeHandle}/>
-      <button onClick={handleClick}> click </button>
-      {
-        loading && <div> loading ....
+    <div className=' min-h-screen bg-[#110F1C]  flex items-center justify-center'>
+      <div className='w-2/4 rounded-md'>
+        <div className='cursor-pointer'>
+          <div {...getRootProps({ className: 'border-2 bg-[#374151] text-white border-[#ccc] border-dashed px-20 py-24 rounded-md flex items-center flex-col' })}>
+            <input {...getInputProps()} />
+            <p>Drag 'n' drop some files here, or click to select files</p>
+            <em>(Only *.apk will be accepted)</em>
           </div>
-      }
-   </div>
+        </div>
+
+        <aside>
+          <h4>Accepted files</h4>
+          <ul>{acceptedFileItems}</ul>
+        </aside>
+        <div className='flex justify-end w-full mt-4'>
+
+        <button disabled={loading} onClick={handleClick} type="button" className="text-white flex bg-blue-800 opacity-90 focus:ring-1 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5">
+          {
+            loading &&  <div className="loader">
+            <div className="face">
+              <div className="circle"></div>
+            </div>
+            <div className="face">
+              <div className="circle"></div>
+            </div>
+          </div>
+          }
+          {
+            loading ? " ... درحال بررسی" :"شروع بررسی"
+          }
+        
+        </button>
+
+        </div>
+
+      </div>
+
+    </div>
+
   )
 }
 
